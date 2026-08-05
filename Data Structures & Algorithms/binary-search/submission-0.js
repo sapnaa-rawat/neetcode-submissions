538d1e7bcfb223343caf51295cec0e233ab333bf
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-08-05 09:58:31 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left =0;
+        let right = nums.length-1;
+        while(left<=right){
+            let mid = left +Math.floor((right-left)/2);
+            if(nums[mid]<target){
+                left= mid+1
+            }
+            else if(nums[mid]>target){
+                right = mid-1
+            }else{
+                return mid
+            }
+        }
+        return -1;
+    }
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
