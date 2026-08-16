1c68cb93e108079939fb39d9466a5693ef8fd693
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-08-16 10:42:05 +0530

    Add: binary-tree-maximum-path-sum - submission-0

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-0.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxPathSum(root) {
+        let MaxSum = -Infinity;
+        function dfs(root){
+            if(!root){
+                return 0;
+            }
+            let leftMax = Math.max(0,dfs(root.left))
+            let rightMax = Math.max(0,dfs(root.right))
+
+            let currentPath = leftMax + rightMax + root.val;
+
+            // Update global maximum
+            MaxSum = Math.max(MaxSum,currentPath)
+
+            return root.val+ Math.max(leftMax, rightMax)    
+        }
+        dfs(root)
+        return MaxSum;
+    }
+}
