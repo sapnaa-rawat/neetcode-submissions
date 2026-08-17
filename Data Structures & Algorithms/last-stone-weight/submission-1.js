e8e668cf5d7738b38aeeb448b04d31367211b386
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-08-17 10:29:58 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        let maxHeap = new MaxPriorityQueue();
+        for (const num of stones) {
+            maxHeap.enqueue(num);
+        }
+        while (maxHeap.size() > 1) {
+            let val1 = maxHeap.dequeue();
+            let val2 = maxHeap.dequeue();
+            if (val1 !== val2) {
+                maxHeap.enqueue(val1 - val2);
+            }
+        }
+        return maxHeap.size() == 1 ? maxHeap.dequeue() : 0;
+    }
+}
