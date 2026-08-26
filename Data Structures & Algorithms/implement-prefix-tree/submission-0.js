c5fe98eaea70c3eb6363fae1d1413c06e001419b
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-08-26 09:49:00 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.js
@@ -0,0 +1,56 @@
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEnd = false;
+    }
+}
+class PrefixTree {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let node = this.root;
+        for(let char of word){
+            if(!node.children[char]){
+                node.children[char] = new TrieNode()
+            }
+            node = node.children[char]
+        }
+        node.isEnd= true
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        let node = this.root;
+        for(let c of word){
+            if(!node.children[c]){
+                return false
+            }
+            node = node.children[c]
+        }
+        return node.isEnd;
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+        let node = this.root;
+        for(let char of prefix){
+            if(!node.children[char]){
+                return false;
+            }
+            node = node.children[char]
+        }
+        return true;
+    }
+}
