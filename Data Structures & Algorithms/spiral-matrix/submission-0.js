4a177d8128b986988869699173b8e50eda832b54
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-08-30 11:14:05 +0530

    Add: spiral-matrix - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     */
+    spiralOrder(matrix) {
+        let res =[];
+        let left = 0;
+        let right = matrix[0].length;
+        let top = 0;
+        let bottom = matrix.length;
+
+        while(left<right && top < bottom){
+            for(let i = left; i< right; i++){
+                res.push(matrix[top][i])
+            }
+            top++;
+        
+        for(let i = top; i < bottom; i++){
+            res.push(matrix[i][right -1])
+        }
+        right--;
+
+        if(!(left<right && top< bottom))
+        break;
+
+        for(let i = right-1; i>=left; i--){
+            res.push(matrix[bottom - 1][i]);
+        }
+        bottom--
+
+        for(let i = bottom-1; i>= top; i--){
+            res.push(matrix[i][left]);
+        }
+        left++;
+    }
+    return res;
+    }
+}
