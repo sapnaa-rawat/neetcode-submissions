0badb4dfb5f3df7bb0d03a2ef2eb7b0dad83e436
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2026-09-01 13:22:54 +0530

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        let count = 0;
+        while(n!== 0){
+            n&= n-1
+            count++
+        }
+        return count;
+    }
+}
